0b8096fd9e710def20c86b89cbec3dde521a5137
matiolsz <matiolsz97@gmail.com>
2024-06-11 23:38:28 +0200

    - visual changes

--- src/app/small-todo/small-todo.component.ts
+++ src/app/small-todo/small-todo.component.ts
@@ -8,18 +8,26 @@ import { TodoService } from '../service/todo.service';
 })
 export class SmallTodoComponent {
 
-  constructor(private todoService: TodoService) {}
+  constructor(private todoService: TodoService) { }
 
   @Input() categoryId = '';
-  
+
   todos!: Array<any>;
 
   ngOnInit(): void {
-  
+
     this.todoService.getTenTodos(this.categoryId).subscribe(val => {
       this.todos = val;
     })
 
+  }
+
+  markComplete(todoId: string) {
+    this.todoService.markComplete(this.categoryId, todoId);
+  }
+
+  markUncomplete(todoId: string) {
+    this.todoService.markUncomplete(this.categoryId, todoId);
   }
 
 
